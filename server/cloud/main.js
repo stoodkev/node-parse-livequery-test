d23225ba8cdd75a6115938c8b322ec9dc5cd3cf0
Stoodkev <quentin.correa@gmail.com>
2017-12-28 21:30:16 +0800

    Adding Scheduled job for deleting old messages

--- server/cloud/main.js
+++ server/cloud/main.js
@@ -2,3 +2,24 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+// Example for Scheduled Job on Parse
+Parse.Cloud.job("scheduleDeleteMessages", function (request, status) {
+  var query = new Parse.Query(Parse.Object.extend("Message"));
+  var d = new Date();
+  d.setDate(d.getDate() - 1);
+  query.lessThan('time',d);
+  query.ascending("createdAt");
+  query.find({
+    useMasterKey: true,
+    success: function(messages) {
+      for(message of messages)
+      {
+          message.destroy({useMasterKey:true});
+      }
+    },
+    error: function(error) {
+      alert("Error: " + error.code + " " + error.message);
+    }
+  });
+});
